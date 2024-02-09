c35631d5f5863996f1445848d39844caec6069d2
Iain-S <25081046+Iain-S@users.noreply.github.com>
2024-02-09 17:27:41 +0000

    Restore an env var

--- run_allgather.sh
+++ run_allgather.sh
@@ -7,6 +7,9 @@
 # "mpi" also works but prints some killed-9 error at the end.
 export CCL_ATL_TRANSPORT=ofi
 
+# If you don't set this, you are told to in a warning.
+export CCL_ZE_IPC_EXCHANGE=sockets
+
 # Pass all arguments to allgather module.
 python allgather.py "${@:1}"
 
